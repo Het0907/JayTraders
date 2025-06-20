11a380b7177b97d0d46493ab61b5892771d10fe3
Parikh Het <148608347+Het0907@users.noreply.github.com>
2025-06-20 10:09:41 +0530

    Wrap App component with GoogleOAuthProvider for authentication support

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = "470526889024-99ug9r4fvg2hooj6a0ohcrv4bnmsht94.apps.googleusercontent.com";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={clientId}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
